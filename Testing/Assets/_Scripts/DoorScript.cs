fc2abebabc39e06d8d883d2c821759240e640702
ahmedrage <ahmedrage@hotmail.com>
2016-05-16 22:43:38 +0930

    Added super basic player movement and door functionality.

---
+++ Testing/Assets/_Scripts/DoorScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorScript : MonoBehaviour {
+	public float range;
+	public GameObject Player;
+	public Transform Target;
+	public int layerMask;
+	// Use this for initialization
+	void Start () {
+		Player = GameObject.FindGameObjectWithTag ("Player");
+		layerMask = 1 << 9;
+		layerMask = ~layerMask;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Raycasting ();
+
+
+	}
+
+	void Raycasting () {
+		RaycastHit hit;
+		if (Physics.Raycast (transform.position, Player.transform.position, out hit, range, layerMask) == true) {
+			Teleport();
+		}
+		print (hit.distance.ToString());
+	}
+
+	void Teleport() {
+		if (Input.GetKeyDown(KeyCode.E)){
+		Player.transform.position = Target.position;
+		}
+	}
+
+
+}
---
+++ Testing/Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public Rigidbody rb;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.velocity = new Vector3 (Input.GetAxis("Vertical") * speed, 0, Input.GetAxis("Horizontal") * -speed);
+	}
+}
